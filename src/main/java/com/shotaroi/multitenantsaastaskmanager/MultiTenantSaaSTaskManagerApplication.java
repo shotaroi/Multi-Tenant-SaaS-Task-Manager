8f36ee2b9cd910712fcad6365acacc02f0cededa
shotaroi <58347058+shotaroi@users.noreply.github.com>
2025-12-31 00:25:40 +0100

    Initial commit

---
+++ src/main/java/com/shotaroi/multitenantsaastaskmanager/MultiTenantSaaSTaskManagerApplication.java
@@ -0,0 +1,13 @@
+package com.shotaroi.multitenantsaastaskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiTenantSaaSTaskManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiTenantSaaSTaskManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shotaroi/multitenantsaastaskmanager/MultiTenantSaaSTaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shotaroi.multitenantsaastaskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiTenantSaaSTaskManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
