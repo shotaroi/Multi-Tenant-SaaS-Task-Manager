44c4962b9960e448ac21227884bb59e0223a0cf5
shotaroi <58347058+shotaroi@users.noreply.github.com>
2026-01-02 14:02:52 +0100

    Update V1__init.sql

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,13 +1,13 @@
+create table organizations (
+    id uuid primary key,
+    name varchar(255) not null,
+    created_at timestamptz not null
+);
+
 create table users (
     id uuid primary key,
     email varchar(255) not null unique,
     password_hash varchar(255) not null,
-    created_at timestamptz not null
-);
-
-create table organizations (
-    id uuid primary key,
-    name varchar(255) not null,
     created_at timestamptz not null
 );
 
