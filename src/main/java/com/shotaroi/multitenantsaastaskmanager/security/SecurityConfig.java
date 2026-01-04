74dc1b1a025c3fd19940e70c8e421b9f7e0507f3
shotaroi <58347058+shotaroi@users.noreply.github.com>
2026-01-04 02:28:04 +0100

    Update SecurityConfig.java

--- src/main/java/com/shotaroi/multitenantsaastaskmanager/security/SecurityConfig.java
+++ src/main/java/com/shotaroi/multitenantsaastaskmanager/security/SecurityConfig.java
@@ -1,11 +1,15 @@
 package com.shotaroi.multitenantsaastaskmanager.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+
 
 @Configuration
 @EnableWebSecurity
@@ -18,12 +22,22 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
                         .anyRequest().authenticated()
                 );
+
         return http.build();
+    }
+
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        // Empty on purpose. We'll replace this with DB + JWT later.
+        return new InMemoryUserDetailsManager();
     }
 }
