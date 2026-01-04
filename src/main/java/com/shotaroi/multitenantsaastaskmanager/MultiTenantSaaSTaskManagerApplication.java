fb98a94d754c6cf67e4c264a2078dce35162a451
shotaroi <58347058+shotaroi@users.noreply.github.com>
2026-01-04 01:50:59 +0100

    Update MultiTenantSaaSTaskManagerApplication.java

--- src/main/java/com/shotaroi/multitenantsaastaskmanager/MultiTenantSaaSTaskManagerApplication.java
+++ src/main/java/com/shotaroi/multitenantsaastaskmanager/MultiTenantSaaSTaskManagerApplication.java
@@ -3,7 +3,8 @@ package com.shotaroi.multitenantsaastaskmanager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.shotaroi.multitenantsaastaskmanager")
+
 public class MultiTenantSaaSTaskManagerApplication {
 
     public static void main(String[] args) {
