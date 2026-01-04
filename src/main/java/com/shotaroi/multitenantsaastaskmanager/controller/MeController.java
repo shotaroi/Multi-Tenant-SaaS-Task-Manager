0ed7c2dec1be47c4e8f3f00387602eb3b7953756
shotaroi <58347058+shotaroi@users.noreply.github.com>
2026-01-04 21:41:38 +0100

    Create MeController.java

---
+++ src/main/java/com/shotaroi/multitenantsaastaskmanager/controller/MeController.java
@@ -0,0 +1,14 @@
+package com.shotaroi.multitenantsaastaskmanager.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MeController {
+
+    @GetMapping("/me")
+    public String me(Authentication authentication) {
+        return "hello " + authentication.getName();
+    }
+}
