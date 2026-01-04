8649e86101bac435eda71f6f3490640cdc1c1596
shotaroi <58347058+shotaroi@users.noreply.github.com>
2026-01-04 01:16:19 +0100

    Create SecurityConfig.java

---
+++ src/main/java/com/shotaroi/multitenantsaastaskmanager/security/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.shotaroi.multitenantsaastaskmanager.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/actuator/health").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults()) // temporary (we’ll remove once JWT is done)
+                .build();
+    }
+}
